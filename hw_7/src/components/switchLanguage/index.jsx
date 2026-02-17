15b8102e884307d34d47fc5e1c590465633123ca
Almira <almiratostova@gmail.com>
2026-02-17 14:20:11 +0100

    added styles

--- hw_7/src/components/switchLanguage/index.jsx
+++ hw_7/src/components/switchLanguage/index.jsx
@@ -1,5 +1,6 @@
 import { useContext } from "react";
 import LanguageContext from "../../context/languageContext";
+import styles from "./styles.module.css";
 
 function SwitchLanguage() {
   const { language, setLanguage } = useContext(LanguageContext);
@@ -9,8 +10,12 @@ function SwitchLanguage() {
   }
 
   return (
-    <div>
-      <h2>{language}</h2>
+    <div className={styles.card}>
+      <h2>
+        {language === "deutsch"
+          ? "Deutsch wurde gewählt"
+          : "Выбран русский язык"}
+      </h2>
       <button onClick={toogleLanguage}>Switch language</button>
     </div>
   );

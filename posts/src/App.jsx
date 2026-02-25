487e01899f7f7e10b5008bf50e054ad338ad9664
Almira <almiratostova@gmail.com>
2026-02-25 13:22:20 +0100

    added postForm

--- posts/src/App.jsx
+++ posts/src/App.jsx
@@ -1,11 +1,13 @@
 import "./App.css";
 import Header from "./components/header";
 import HomePage from "./components/pages/homePage";
+import PostForm from "./components/postForm";
 
 function App() {
   return (
     <>
       <HomePage></HomePage>
+      <PostForm></PostForm>
     </>
   );
 }
--- posts/src/components/postForm/index.jsx
+++ posts/src/components/postForm/index.jsx
@@ -1,7 +1,48 @@
-import styles from "./styles.module css";
+import styles from "./styles.module.css";
+import { useForm } from "react-hook-form";
+import axios from "axios";
+import PersonLogo from "../../assets/person.svg";
+
+const POSTS_URL = "ttps://699eb2fe78dda56d396b07cc.mockapi.io/posts";
 
 function PostForm() {
-  return <div></div>;
+  const { register, handleSubmit, reset } = useForm();
+
+  const onSubmit = async (data) => {
+    try {
+      await axios.post(POSTS_URL, data);
+      reset();
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <div className={styles.wrapper}>
+      <h1 className={styles.title}>Написать пост</h1>
+
+      <form className={styles.card} onSubmit={handleSubmit(onSubmit)}>
+        <div className={styles.avatar}>
+          {" "}
+          <img src={PersonLogo}></img>
+        </div>
+
+        <div className={styles.fields}>
+          <label>
+            <span>Заголовок</span>
+            <input type="text" placeholder="Заголовок" {...register("title")} />
+          </label>
+
+          <label>
+            <span>Текст поста</span>
+            <textarea placeholder="Введите текст..." {...register("content")} />
+          </label>
+
+          <button type="submit">Публикация</button>
+        </div>
+      </form>
+    </div>
+  );
 }
 
 export default PostForm;

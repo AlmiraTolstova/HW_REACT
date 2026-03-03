b3b0155a1b6f4ff7a71b102c148582e9f2f5e3b0
Almira <almiratostova@gmail.com>
2026-03-03 19:39:54 +0100

    first commit

---
+++ hw_11/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+
+import "./App.css";
+import { Routes } from "react-router-dom";
+
+function App() {
+  return (
+    <div>
+      <Routes></Routes>
+    </div>
+  );
+}
+
+export default App;

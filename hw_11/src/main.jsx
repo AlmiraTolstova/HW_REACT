ef2bc78d7b99937652e27104d93afa406f1777ec
Almira <almiratostova@gmail.com>
2026-03-03 20:39:38 +0100

    added contextProvider

--- hw_11/src/main.jsx
+++ hw_11/src/main.jsx
@@ -3,9 +3,12 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App.jsx";
+import { ArticleProvider } from "./components/articleProvider/index.jsx";
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-    <App />
+    <ArticleProvider>
+      <App />
+    </ArticleProvider>
   </BrowserRouter>,
 );
--- hw_11/src/pages/articlePage/index.jsx
+++ hw_11/src/pages/articlePage/index.jsx
@@ -1,23 +1,16 @@
+import { useContext } from "react";
 import { useNavigate, useParams } from "react-router-dom";
+import ArticleContext from "../../context/articleContext";
 
 function ArticlePage() {
   const { id } = useParams();
   const navigate = useNavigate();
+  const { articles } = useContext(ArticleContext);
 
   return (
     <div>
-      <h1>Страница отдельной статьи {id}</h1>
-      <p>
-        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy
-        eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam
-        voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet
-        clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit
-        amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
-        nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,
-        sed diam voluptua. At vero eos et accusam et justo duo dolores et ea
-        rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem
-        ipsum dolor sit amet.
-      </p>
+      <h1> {articles[id].title}</h1>
+      <p>{articles[id].text}</p>
       <button
         onClick={() => {
           navigate("/articleslist");

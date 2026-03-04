792ce5c593b1bbf39f3e0cd2c58de569c73cd7e0
Almira <almiratostova@gmail.com>
2026-03-04 13:02:13 +0100

    variables for ToggleText

---
+++ react_29/src/components/toggleText/index.jsx
@@ -0,0 +1,11 @@
+import styles from "./styles.module.css";
+import { useRef, useState } from "react";
+
+function ToggleText() {
+  const textRef = useRef(null);
+  const [isVisible, setIsVisible] = useState();
+  const [duration, setDuration] = useState();
+  return <div></div>;
+}
+
+export default ToggleText;

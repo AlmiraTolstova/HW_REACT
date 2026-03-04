09cbfd268f69145989dad43a663152f014683b25
Almira <almiratostova@gmail.com>
2026-03-04 13:14:33 +0100

    function forToggleText

--- react_29/src/App.jsx
+++ react_29/src/App.jsx
@@ -2,9 +2,14 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import ToggleText from "./components/toggleText";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <ToggleText></ToggleText>
+    </>
+  );
 }
 
 export default App;

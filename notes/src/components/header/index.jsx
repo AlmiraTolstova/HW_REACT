5f3e24be8fed4b42ba8da256f586c2a1c8092e4a
Almira <almiratostova@gmail.com>
2026-03-11 10:14:05 +0100

    added header,footer

---
+++ notes/src/components/header/index.jsx
@@ -0,0 +1,12 @@
+import styles from "./styles.module.css";
+
+function Header() {
+  return (
+    <header className={styles.header}>
+      {" "}
+      <h1>My notes App</h1>
+    </header>
+  );
+}
+
+export default Header;

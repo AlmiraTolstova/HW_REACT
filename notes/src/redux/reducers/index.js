941a75fb8d493b1ca8a2458f3944804f8e40a513
Almira <almiratostova@gmail.com>
2026-03-11 10:51:47 +0100

    added reducers,actions

--- notes/src/redux/reducers/index.js
+++ notes/src/redux/reducers/index.js
@@ -1,9 +1,40 @@
+import { ADD_NOTE, DELETE_NOTE, EDIT_NOTE } from "../actions";
+
 const initialState = {
   todos: [],
 };
 
 const todoReducer = (state, action) => {
-  return state;
+  switch (action.type) {
+    case ADD_NOTE:
+      return {
+        ...state,
+        todos: [...state.todos, action.payload],
+      };
+
+    case DELETE_NOTE:
+      return {
+        ...state,
+        todos: state.todos.filter((note) => note.id !== action.payload),
+      };
+
+    case EDIT_NOTE:
+      return {
+        ...state,
+        todos: state.todos.map((note) =>
+          note.id === action.payload.id
+            ? {
+                ...note,
+                title: action.payload.title,
+                text: action.payload.text,
+              }
+            : note,
+        ),
+      };
+
+    default:
+      return state;
+  }
 };
 
 export default todoReducer;

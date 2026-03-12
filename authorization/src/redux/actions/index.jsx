059b3f463f9d507e529f2fcfa332e618041204d5
Almira <almiratostova@gmail.com>
2026-03-12 19:53:48 +0100

    authorization

---
+++ authorization/src/redux/actions/index.jsx
@@ -0,0 +1,15 @@
+export const LOGIN = "LOGIN";
+export const LOGOUT = "LOGOUT";
+
+export const login = (username) => {
+  return {
+    type: LOGIN,
+    payload: username,
+  };
+};
+
+export const logout = () => {
+  return {
+    type: LOGOUT,
+  };
+};

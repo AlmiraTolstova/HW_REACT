d4432ecdfd758e9ee879ee6cdca173c79adb9a03
Almira <almiratostova@gmail.com>
2026-03-20 11:51:38 +0100

    debugging states

--- profile_card/src/components/profilePlayground/index.jsx
+++ profile_card/src/components/profilePlayground/index.jsx
@@ -1,17 +1,256 @@
 import { useState } from "react";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
-import CardMedia from "@mui/material/CardMedia";
-import CardActionArea from "@mui/material/CardActionArea";
 import Avatar from "@mui/material/Avatar";
 import Button from "@mui/material/Button";
 import TextField from "@mui/material/TextField";
+import Stack from "@mui/material/Stack";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import CardActions from "@mui/material/CardActions";
+import Slider from "@mui/material/Slider";
+import Select from "@mui/material/Select";
+import MenuItem from "@mui/material/MenuItem";
+import Radio from "@mui/material/Radio";
+import RadioGroup from "@mui/material/RadioGroup";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import FormControl from "@mui/material/FormControl";
+import Switch from "@mui/material/Switch";
+import Checkbox from "@mui/material/Checkbox";
+import Alert from "@mui/material/Alert";
+import Chip from "@mui/material/Chip";
 
 function ProfilePlayground() {
   const [profileSettings, setProfileSettings] = useState({
     name: "Anna",
     surname: "Ivanova",
+    avatarSize: 80,
+    buttonColor: "primary",
+    buttonSize: "medium",
+    isOnline: false,
+    cardVariant: "elevation",
+    showAlert: false,
+    profession: "Разработчик",
   });
-  return <div></div>;
+
+  return (
+    <Stack direction="row" spacing={4} sx={{ p: 4 }}>
+      {/* PERSON CARD */}
+      <Box flex={1}>
+        <Typography variant="h6">Карточка профиля</Typography>
+
+        <Card
+          variant={
+            profileSettings.cardVariant === "outlined" ? "outlined" : undefined
+          }
+          sx={{
+            transition: "0.3s",
+            "&:hover": {
+              transform: "translateY(-5px)",
+              boxShadow: 6,
+            },
+          }}
+        >
+          <CardContent>
+            <Avatar
+              sx={{
+                width: profileSettings.avatarSize,
+                height: profileSettings.avatarSize,
+                transition: "0.3s",
+              }}
+            >
+              {profileSettings.name[0]}
+            </Avatar>
+
+            <Typography variant="h6">
+              {profileSettings.isOnline ? "● " : "○ "}
+              {profileSettings.name} {profileSettings.surname}
+            </Typography>
+
+            <Typography color={profileSettings.isOnline ? "green" : "gray"}>
+              {profileSettings.isOnline ? "Онлайн" : "Офлайн"}
+            </Typography>
+
+            <Chip label={profileSettings.profession} sx={{ mt: 1 }} />
+          </CardContent>
+
+          <CardActions>
+            <Button
+              variant="contained"
+              color={profileSettings.buttonColor}
+              size={profileSettings.buttonSize}
+            >
+              Написать
+            </Button>
+            <Button
+              variant="outlined"
+              color={profileSettings.buttonColor}
+              size={profileSettings.buttonSize}
+            >
+              Предложить работу
+            </Button>
+          </CardActions>
+
+          {profileSettings.showAlert && (
+            <Alert sx={{ m: 2 }} severity="info">
+              Идет поиск стажеров
+            </Alert>
+          )}
+        </Card>
+      </Box>
+
+      {/* --------SETTINGS CARD------- */}
+      <Box flex={1}>
+        <Typography variant="h6">Настройки</Typography>
+
+        <Card sx={{ p: 2 }}>
+          <Box sx={{ display: "flex", flexDirection: "column", gap: 2 }}>
+            <TextField
+              label="Имя"
+              value={profileSettings.name}
+              onChange={(e) =>
+                setProfileSettings({
+                  ...profileSettings,
+                  name: e.target.value,
+                })
+              }
+            />
+
+            <TextField
+              label="Фамилия"
+              value={profileSettings.surname}
+              onChange={(e) =>
+                setProfileSettings({
+                  ...profileSettings,
+                  surname: e.target.value,
+                })
+              }
+            />
+
+            <Slider
+              value={profileSettings.avatarSize}
+              min={40}
+              max={150}
+              onChange={(e, value) =>
+                setProfileSettings({
+                  ...profileSettings,
+                  avatarSize: value,
+                })
+              }
+            />
+
+            <Select
+              value={profileSettings.profession}
+              onChange={(e) =>
+                setProfileSettings({
+                  ...profileSettings,
+                  profession: e.target.value,
+                })
+              }
+            >
+              <MenuItem value="Разработчик">Разработчик</MenuItem>
+              <MenuItem value="Дизайнер">Дизайнер</MenuItem>
+              <MenuItem value="Менеджер">Менеджер</MenuItem>
+              <MenuItem value="Аналитик">Аналитик</MenuItem>
+            </Select>
+
+            <FormControl>
+              <RadioGroup
+                row
+                value={profileSettings.buttonColor}
+                onChange={(e) =>
+                  setProfileSettings({
+                    ...profileSettings,
+                    buttonColor: e.target.value,
+                  })
+                }
+              >
+                <FormControlLabel
+                  value="primary"
+                  control={<Radio />}
+                  label="Primary"
+                />
+                <FormControlLabel
+                  value="secondary"
+                  control={<Radio />}
+                  label="Secondary"
+                />
+                <FormControlLabel
+                  value="success"
+                  control={<Radio />}
+                  label="Success"
+                />
+                <FormControlLabel
+                  value="error"
+                  control={<Radio />}
+                  label="Error"
+                />
+              </RadioGroup>
+            </FormControl>
+
+            <FormControl>
+              <RadioGroup
+                row
+                value={profileSettings.buttonSize}
+                onChange={(e) =>
+                  setProfileSettings({
+                    ...profileSettings,
+                    buttonSize: e.target.value,
+                  })
+                }
+              >
+                <FormControlLabel
+                  value="small"
+                  control={<Radio />}
+                  label="Small"
+                />
+                <FormControlLabel
+                  value="medium"
+                  control={<Radio />}
+                  label="Medium"
+                />
+                <FormControlLabel
+                  value="large"
+                  control={<Radio />}
+                  label="Large"
+                />
+              </RadioGroup>
+            </FormControl>
+
+            <FormControlLabel
+              control={
+                <Switch
+                  checked={profileSettings.isOnline}
+                  onChange={(e) =>
+                    setProfileSettings({
+                      ...profileSettings,
+                      isOnline: e.target.checked,
+                    })
+                  }
+                />
+              }
+              label="Онлайн статус"
+            />
+
+            <FormControlLabel
+              control={
+                <Checkbox
+                  checked={profileSettings.showAlert}
+                  onChange={(e) =>
+                    setProfileSettings({
+                      ...profileSettings,
+                      showAlert: e.target.checked,
+                    })
+                  }
+                />
+              }
+              label="Показать Alert"
+            />
+          </Box>
+        </Card>
+      </Box>
+    </Stack>
+  );
 }
+
 export default ProfilePlayground;

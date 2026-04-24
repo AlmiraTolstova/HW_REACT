e42cb77b1bc582431571542eb8287f2c3a22f415
Almira <almiratostova@gmail.com>
2026-04-24 18:22:08 +0200

    add task 23

---
+++ jwt_auth_client/src/pages/profile/index.jsx
@@ -0,0 +1,35 @@
+import styles from "./styles.module.css";
+import { jwtDecode } from "jwt-decode";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router";
+import { logout } from "../../redux/slices/authSlice";
+
+function Profile() {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const token = localStorage.getItem("token");
+  const tokenData = token ? jwtDecode(token) : null;
+
+  const handleLogout = () => {
+    dispatch(logout());
+    navigate("/login");
+  };
+  return (
+    <div className={styles.container}>
+      <h1>
+        {tokenData ? (
+          <span>
+            {tokenData.user.id} {tokenData.user.email}
+          </span>
+        ) : (
+          "Login inot account"
+        )}
+      </h1>
+      <button onClick={handleLogout} className={styles}>
+        {tokenData ? "Logout" : "Login"}
+      </button>
+    </div>
+  );
+}
+
+export default Profile;
